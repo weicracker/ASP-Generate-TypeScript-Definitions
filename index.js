a885969b0180194e1f6519d0ed7ec7e7604b2abc
jiwei <jiwei3778@bjsasc.com>
2017-07-13 20:16:28 +0800

    更新命令行提示

--- index.js
+++ index.js
@@ -2,20 +2,27 @@
 const fs = require("fs");
 const Mustache = require('mustache');
 const _ = require('lodash');
+const args = require('minimist')(process.argv.slice(2));
+// 从命令行获取 文件路径// 默认去，走测试临时文件
+let filePath = args._[0]||"./template/tmp.json";
 
-
-let filePath = "./template/tmp.json";
 let tmplPath = "./template/tsd.tmp.mustache";
 let typePath = "./template/type.mustache";
-
-let viewJson = JSON.parse(fs.readFileSync(filePath, 'UTF-8'));
+console.log("开始读取json");
+try{
+    let viewJson = JSON.parse(fs.readFileSync(filePath, 'UTF-8'));
+}catch(e){
+    console.log("JSON文件不正确，请检查！")
+    process.exit(0);
+}
 let viewTmpl = fs.readFileSync(tmplPath,"UTF-8");
 let viewType = fs.readFileSync(typePath,"UTF-8");
-
+console.log("正在生成代码");
 let tsdSourceCode = Mustache.render(viewTmpl,viewJson,{
     type:viewType
 })
 let fileName = viewJson.className;
 let outFilePath = `${__dirname}/${fileName}.d.ts`;
 fs.writeFileSync(outFilePath,tsdSourceCode,"UTF-8");
+console.log(`${__dirname}/${fileName}.d.ts，文件创建成功！`);
 process.exit(0);
